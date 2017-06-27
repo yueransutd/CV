9f3fb9254584bc580d58358841fb2405d7dcb2e1
yueransutd <1055538755@qq.com>
2017-06-28 00:02:56 +0800

    Modification of another code

---
+++ find.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+#Modifications from the code of https://www.youtube.com/watch?v=OnWIYI6-4Ss&t=2783s
+
+
+from __future__ import division
+import numpy as np
+from math import cos,sin
+import scipy
+from matplotlib import pyplot as plt
+import cv2
+
+green=(0,255,0)
+def show(image):
+    
+    #figure size in inches
+    plt.figure(figsize=(10,10))
+    plt.imshow(image,interpolation='nearest')
+    
+def overlay_mask(mask,image):
+    #make the mask rgb
+    rgb_mask=cv2.cvtColor(mask,cv2.COLOR_GRAY2BGR)
+    
+    #add tgt
+    img=cv2.addWeighted(rgb_mask,0.5,image,0.5,0)
+    return img
+
+def find_biggest_contour(image):
+    #copy image
+    image=image.copy()
+    
+    image,coutours,hierachy=cv2.findContours(image,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    #isolating the largest contour
+    contour_sizes=[(cv2.contourArea(contour),contour) for contour in coutours]
+    biggest_contour=max(contour_sizes,key=lambda x:x[0])[1]
+    
+    #return the biggest contour
+    mask=np.zeros(image.shape,np.uint8)
+    cv2.drawContours(mask,[biggest_contour],-1,255,-1)
+    return biggest_contour,mask
+
+def circle_contour(image,contour):
+    #bounding ellipse
+    image_with_ellipse=image.copy()
+    ellipse=cv2.fitEllipse(contour)
+    
+    #add it
+    cv2.ellipse(image_with_ellipse,ellipse,green,2,cv2.CV_AA)
+    return image_with_ellipse
+
+
+    
+def find_strawberry(image):
+    #BGR: red green blue
+    #BGR: blue green red
+    #convert to the correct color scheme
+    image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    
+    #scale our image properly
+    max_dimension=max(image.shape)
+    scale=700/max_dimension
+    image=cv2.resize(image,None,fx=scale,fy=scale)
+    
+    #clean our image
+    image_blur=cv2.GaussianBlur(image,(7,7),0)
+    image_blur_hsv=cv2.cvtColor(image_blur,cv2.COLOR_RGB2HSV)
+    
+    #define filters
+    #filter by the color
+    min_red=np.array([0,100,80])
+    max_red=np.array([10,256,256])
+    
+    mask1=cv2.inRange(image_blur_hsv,min_red,max_red)
+    
+    #filter by brightness
+    min_red2=np.array([170,100,80])
+    max_red2=np.array([180,256,256])
+    
+    mask2=cv2.inRange(image_blur_hsv,min_red2,max_red2)
+    
+    #take two masks and 
+    #combine masks
+    mask=mask1+mask2
+    
+    #segmentation
+    kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
+    mask_closed=cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
+    mask_clean=cv2.morphologyEx(mask_closed,cv2.MORPH_OPEN,kernel)
+    
+    #find the biggest strawberry
+    big_strawberry_contour,mask_strawberries=find_biggest_contour(mask_clean)
+    
+    #overlay the masks that we created on the image
+    overlay=overlay_mask(mask_clean,image)
+    
+    #circle the biggest strawberry
+    circled=circle_contour(overlay, big_strawberry_contour)
+    show(circled)
+    
+    #convert back to original color scheme
+    bgr=cv2.cvtColor(circled,cv2.COLOR_RGB2BGR)
+    
+    return bgr
+
+
+#read the image
+image=cv2.imread('berry.jpg')
+result=find_strawberry (image)   
+#write the new image
+cv2.imwrite('berry2.jpg',result)    
+    
+    
+    
+
